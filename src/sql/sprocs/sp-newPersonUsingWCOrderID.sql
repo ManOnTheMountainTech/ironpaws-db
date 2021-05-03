36bb1162124426c7caf6a35167ae8b6593d34f89
Bryan Young <mindinsky@gmail.com>
2021-05-03 08:46:27 -0700

    remove locale as wordpress has this

--- src/sql/sprocs/sp-newPersonUsingWCOrderID.sql
+++ src/sql/sprocs/sp-newPersonUsingWCOrderID.sql
@@ -7,8 +7,8 @@ CREATE  PROCEDURE `sp_newPersonUsingWCOrderID`(IN salutation VARCHAR(255), IN wc
     SQL SECURITY INVOKER
     COMMENT 'Inserts into people using wc_customerId to sparsly populate'
 BEGIN
-	INSERT INTO people (`salutation`, `wc_customer_id`, `language`) 
-    VALUES (salutation, wc_customerId, argLanguage);
+	INSERT INTO people (`salutation`, `wc_customer_id`) 
+    VALUES (salutation, wc_customerId);
 	SELECT LAST_INSERT_ID();
 END$$
 
