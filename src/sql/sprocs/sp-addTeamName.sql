22bef7ac913eab11330073f3bfea0c7d76eae5dd
Bryan Young <mindinsky@gmail.com>
2021-10-10 14:39:53 -0700

    add backticks to param

--- src/sql/sprocs/sp-addTeamName.sql
+++ src/sql/sprocs/sp-addTeamName.sql
@@ -1,12 +1,12 @@
 DROP procedure IF EXISTS `sp_addTeamName`;
 
 DELIMITER $$
-CREATE DEFINER=`bryan`@`localhost` PROCEDURE `sp_addTeamName` (IN teamName VARCHAR(255))
-COMMENT 'Adds a team name'
-SQL SECURITY DEFINER;
+CREATE DEFINER=`bryan`@`localhost` PROCEDURE `sp_addTeamName`(IN `teamName` VARCHAR(255))
+    MODIFIES SQL DATA
+    DETERMINISTIC
+    COMMENT 'Adds a team name'
 BEGIN
     INSERT INTO team_name (`name_tn`) VALUES (teamName);
     SELECT LAST_INSERT_ID();
 END$$
-
 DELIMITER ;
