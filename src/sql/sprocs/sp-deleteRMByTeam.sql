a5f5c8f7b3138914dad00bd4d7fa259edf14f9f3
Bryan Young <mindinsky@gmail.com>
2021-10-10 11:52:49 -0700

    Fix STRICT_MODE complaining about no temp table columns.

--- src/sql/sprocs/sp-deleteRMByTeam.sql
+++ src/sql/sprocs/sp-deleteRMByTeam.sql
@@ -1,12 +1,14 @@
 DROP procedure IF EXISTS `sp_deleteRMByTeam`;
 
 DELIMITER $$
-CREATE DEFINER=`bryan`@`localhost` PROCEDURE `sp_deleteRMByTeam`()
+CREATE DEFINER=`bryan`@`localhost` PROCEDURE `sp_deleteRM`(IN person_id INT)
     READS SQL DATA
     SQL SECURITY DEFINER
-    COMMENT 'Deletes the specified race manager given a person ID. v 0.001'
+    COMMENT 'Deletes the specified race manager given a person ID. v 0.002'
 BEGIN        
-        #SELECT * FROM team_clones;
+    DELETE FROM race_managers WHERE (rm_person_fk = person_id);
+
+    /*        #SELECT * FROM team_clones;
         #SELECT * FROM race_instances;
         #SELECT * FROM race_managers;
 
@@ -14,7 +16,7 @@ BEGIN
             SELECT rm_id_ai FROM (
             (SELECT rm_id_ai FROM team_clones tc
                 JOIN race_instances ri ON (ri_team_fk = tc.team_id)
-                JOIN race_managers rm ON (rm.rm_race_instances_fk = ri.id_ri_ai)) as qo)));
+                JOIN race_managers rm ON (rm.rm_race_instances_fk = ri.id_ri_ai)) as qo))); */
 END$$
 
 DELIMITER ;
--- src/sql/sprocs/sp-deleteTRSE.sql
+++ src/sql/sprocs/sp-deleteTRSE.sql
@@ -1,15 +1,16 @@
 DROP procedure IF EXISTS `sp_deleteTRSE`;
 
 DELIMITER $$
-CREATE DEFINER=`bryan`@`localhost` PROCEDURE `sp_deleteTRSE`()
+CREATE DEFINER=`bryan`@`localhost` PROCEDURE `sp_deleteTRSE`(IN personId INT)
     READS SQL DATA
     SQL SECURITY DEFINER
-    COMMENT 'Deletes the specified race stage instance given a person ID. v 0.003'
+    COMMENT 'Deletes the specified race stage instance given a person ID. v 0.004'
 BEGIN        
-        DELETE FROM team_race_stage_entry WHERE (trse_team_fk IN (
-            SELECT team_id FROM (
-            (SELECT team_id FROM team_clones tc
-                JOIN team_race_stage_entry trse ON (trse.trse_team_fk = tc.team_id)) as qo)));
-END$$
+    CALL sp_getTeamsFromPersonIdAsTable(personId);
 
+    DELETE FROM team_race_stage_entry WHERE (trse_team_fk IN ( 
+            SELECT team_id FROM 
+                (SELECT team_id FROM team_clones tc 
+                JOIN team_race_stage_entry trse ON (trse.trse_team_fk = tc.team_id)) as qo));
+END$$
 DELIMITER ;
